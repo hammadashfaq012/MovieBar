2b1d405110077fd4edc07d4ca7d111018cb6caa3
Hammad-Ashfaq <hammdashfaq.023@gmail.com>
2026-08-19 02:07:53 +0500

    Updated project

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import SearchBar from '../../components/SearchBar/SearchBar'
 import ShowGrid from '../../components/ShowGrid/ShowGrid'
-import { searchShows, getShows } from '../../api/api'
+import { getShows } from '../../api/api'
 import './Home.css'
 
 function Home({ favorites, addFavorite, removeFavorite }) {
